b5bfc8c1b466dc7eabe8479d75e390e2443224ff
Anurag singh <anuragz.v1@gmail.com>
2020-06-18 06:01:04 +0530

    Added optimised Approach

--- Prerequisites/Target Marbles.java
+++ Prerequisites/Target Marbles.java
@@ -12,22 +12,49 @@ public class Main {
         for(int i =0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        for(int i =0;i<n;i++){
-            for(int j = i;j<n;j++){
-                int sum = 0;
-                String s = "";
-                for(int k = i;k<=j;k++){
-                    sum+=arr[k];
-                    s+= " "+arr[k];
+        // for(int i =0;i<n;i++){
+        //     for(int j = i;j<n;j++){
+        //         int sum = 0;
+        //         String s = "";
+        //         for(int k = i;k<=j;k++){
+        //             sum+=arr[k];
+        //             s+= " "+arr[k];
+        //         }
+        //         if(sum == target){
+        //             System.out.println("true");
+        //             System.out.println(s.trim());
+        //             return;
+        //         }
+        //     }
+        // }
+        // System.out.println("false");
+        
+        
+        
+        //Optimised approach
+        
+        int start = 0;
+        int end = 0;
+        int sum = arr[0];
+        while(start<arr.length && end <arr.length){
+            if(sum<target){
+                end++;
+                sum+=arr[end];
+            }
+            else if(sum >target){
+                sum-=arr[start];
+                start++;
+            }
+            else{
+                System.out.println("true");
+                for(int k = start ;k<=end;k++){
+                    System.out.print(arr[k]+" ");
                 }
-                if(sum == target){
-                    System.out.println("true");
-                    System.out.println(s.trim());
-                    return;
-                }
+                return;
             }
         }
-        System.out.println("false");
+        
+        System.out.print("false");
     }
 
 }
